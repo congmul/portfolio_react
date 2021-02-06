202d740b2212f8b76146592fd35692c79c991217
Jehyun <congmul@hotmail.com>
2021-02-06 01:55:30 -0800

    Footer

--- src/components/FooterComponent/index.jsx
+++ src/components/FooterComponent/index.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <footer className="myfooter">
             <div className="footerDivider"></div>
-            <p>© 2020-2021 Copyright Wavemg corp.</p>
+            <p>© 2020-2021 Copyright Jehyun Jung</p>
         </footer>
     )
 }
